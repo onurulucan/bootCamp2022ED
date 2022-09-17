8aecc1bc356d0450efed8a22fc169b4ce0cd8100
Onur Ulucan <bou@anadolu.edu.tr>
2022-09-17 07:28:38 +0300

    first week

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //For döngüsü
+        for (int i=2; i<10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü Bitti.");
+
+        int i=1;
+        //While Döngüsü
+        while (i<10) {
+
+        }
+
+
+    }
+}
---
+++ recapDemo1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi1 = 20;
+        int sayi2 = 25;
+        int sayi3 = 2;
+        int enBuyuk = sayi1;
+
+    if(enBuyuk<sayi2) {
+        enBuyuk = sayi2;
+        System.out.println(enBuyuk);
+    }else if(sayi2<sayi3){
+        enBuyuk = sayi3;
+        System.out.println(enBuyuk);
+
+    }else{
+        System.out.println(enBuyuk);
+
+    }
+
+
+
+    }
+}
